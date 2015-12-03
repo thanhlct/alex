8f66859d0c2c746ce78cb509204bbd6dce8e14a2
thanhlct <thanhlct@gmail.com>
2015-12-03 09:56:07 +0100

    Backup

---
+++ alex/components/simulator/user_simulator/backup/main.py
@@ -0,0 +1,22 @@
+import autopath
+from alex.components.simulator.simple_user_simulator import SimpleUserSimulator
+from alex.utils.database.python_database import PythonDatabase
+from alex.components.slu.da import DialogueActItem, DialogueAct
+
+def get_config():
+    global cfg
+    #pdb.set_trace()
+    cfg = Config.load_configs(['../../../applications/PublicTransportInfoEN/ptien.cfg', '../../../applications/PublicTransportInfoEN/simulator.cfg'])
+    cfg['Logging']['system_logger'].info("Voip Hub\n" + "=" * 120)
+
+def simulate_one_dialogue():
+    pass
+
+def main():
+    get_config()
+    db = PythonDatabase(cfg)
+    user = SimpleUserSimulator(cfg, db)
+    simulate_one_dialogue()
+
+if __name__ == '__main__':
+    main()
